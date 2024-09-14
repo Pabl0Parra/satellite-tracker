f00a4428559088314dceb5e1317891818e5b6d14
Pabl0Parra <pabsblue16@gmail.com>
2024-09-14 12:40:45 +0200

    start project

--- App.js
+++ App.js
@@ -1,211 +1,233 @@
 import React, { Component } from 'react';
-import "./assets/theme.css";
+import './assets/theme.css';
 import { Engine } from './engine';
 import Info from './Info';
 import Search from './Search/Search';
 import SelectedStations from './Selection/SelectedStations';
-import Fork from './fork';
 import * as qs from 'query-string';
 import Highlights from './highlights';
 import DateSlider from './Options/DateSlider';
 
 // Some config
 const UseDateSlider = false;
-const DateSliderRangeInMilliseconds = 24 * 60 * 60 * 1000;  // 24 hours
+const DateSliderRangeInMilliseconds = 24 * 60 * 60 * 1000; // 24 hours
 
 // Bypass CORS
 function getCorsFreeUrl(url) {
-    return url;
+  return url;
 }
 
 class App extends Component {
+  state = {
+    selected: [],
+    stations: [],
+    query: null,
+    queryObjectCount: 0,
+    initialDate: new Date().getTime(),
+    currentDate: new Date().getTime(),
+    referenceFrame: UseDateSlider ? 2 : 1,
+  };
 
-    state = {
-        selected: [],
-        stations: [], 
-        query: null,
-        queryObjectCount: 0,
-        initialDate: new Date().getTime(),
-        currentDate: new Date().getTime(), 
-        referenceFrame: UseDateSlider ? 2 : 1
+  componentDidMount() {
+    this.engine = new Engine();
+    this.engine.referenceFrame = this.state.referenceFrame;
+    this.engine.initialize(this.el, {
+      onStationClicked: this.handleStationClicked,
+    });
+    this.addStations();
+
+    this.engine.updateAllPositions(new Date());
+
+    setInterval(this.handleTimer, 1000);
+  }
+
+  componentWillUnmount() {
+    this.engine.dispose();
+  }
+
+  processQuery = (stations) => {
+    const q = window.location.search;
+    if (!q) return;
+
+    const params = qs.parse(q);
+
+    if (params.ss) {
+      const selectedIds = params.ss.split(',');
+      if (!selectedIds || selectedIds.length === 0) return;
+
+      selectedIds.forEach((id) => {
+        const station = this.findStationById(stations, id);
+        if (station) this.selectStation(station);
+      });
     }
 
-    componentDidMount() {
-        this.engine = new Engine();
-        this.engine.referenceFrame = this.state.referenceFrame;
-        this.engine.initialize(this.el, {
-            onStationClicked: this.handleStationClicked
-        });
-        this.addStations();
-
-        this.engine.updateAllPositions(new Date())
-
-        setInterval(this.handleTimer, 1000);
+    if (params.highlight) {
+      const query = params.highlight;
+      const matches = this.queryStationsByName(stations, query);
+      matches.forEach((st) => this.engine.highlightStation(st));
+      this.setState({ ...this.state, query, queryObjectCount: matches.length });
     }
+  };
 
-    componentWillUnmount() {
-        this.engine.dispose();
-    }
+  queryStationsByName = (stations, query) => {
+    query = query.toLowerCase();
+    return stations.filter((st) => st.name.toLowerCase().indexOf(query) > -1);
+  };
 
-    processQuery = (stations) => {
-        const q = window.location.search;
-        if (!q) return;
+  findStationById = (stations, id) => {
+    return stations.find((st) => st.satrec && st.satrec.satnum == id);
+  };
 
-        const params = qs.parse(q);
+  handleStationClicked = (station) => {
+    if (!station) return;
 
-        if (params.ss) {
-            const selectedIds = params.ss.split(',');
-            if (!selectedIds || selectedIds.length === 0) return;
+    this.toggleSelection(station);
+  };
 
-            selectedIds.forEach(id => {
-                const station = this.findStationById(stations, id);
-                if (station) this.selectStation(station);
-            });
-        }
+  toggleSelection(station) {
+    if (this.isSelected(station)) this.deselectStation(station);
+    else this.selectStation(station);
+  }
 
-        if (params.highlight) {
-            const query = params.highlight;
-            const matches = this.queryStationsByName(stations, query);
-            matches.forEach(st => this.engine.highlightStation(st));
-            this.setState({...this.state, query, queryObjectCount: matches.length });
-        }
-    }
+  isSelected = (station) => {
+    return this.state.selected.includes(station);
+  };
 
-    queryStationsByName = (stations, query) => {
-        query = query.toLowerCase();
-        return stations.filter(st => st.name.toLowerCase().indexOf(query) > -1)
-    }
+  selectStation = (station) => {
+    const newSelected = this.state.selected.concat(station);
+    this.setState({ selected: newSelected });
 
-    findStationById = (stations, id) => {
-        return stations.find(st => st.satrec && st.satrec.satnum == id);
-    }
+    this.engine.addOrbit(station);
+  };
 
-    handleStationClicked = (station) => {
-        if (!station) return;
+  deselectStation = (station) => {
+    const newSelected = this.state.selected.filter((s) => s !== station);
+    this.setState({ selected: newSelected });
 
-        this.toggleSelection(station);
-    }
+    this.engine.removeOrbit(station);
+  };
 
-    toggleSelection(station) {
-        if (this.isSelected(station))
-            this.deselectStation(station);
-        else
-            this.selectStation(station);
-    }
+  addStations = () => {
+    this.addCelestrakSets();
+    //this.engine.addSatellite(ISS);
+    //this.addAmsatSets();
+  };
 
-    isSelected = (station) => {
-        return this.state.selected.includes(station);
-    }
+  addCelestrakSets = () => {
+    //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/weather.txt'), 0x00ffff)
+    //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/cosmos-2251-debris.txt'), 0xff0090)
+    this.engine
+      .loadLteFileStations(
+        getCorsFreeUrl(
+          'https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle',
+        ),
+        0xffffff,
+      )
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/science.txt'), 0xffff00)
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/stations.txt'), 0xffff00)
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/iridium-NEXT.txt'), 0x00ff00)
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/gps-ops.txt'), 0x00ff00)
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/ses.txt'), 0xffffff)
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/starlink.txt'), 0x0000ff)
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/gps-ops.txt'), 0xffffff, { orbitMinutes: 0, satelliteSize: 200 })
+      //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/glo-ops.txt'), 0xff0000, { orbitMinutes: 500, satelliteSize: 500 })
+      .then((stations) => {
+        this.setState({ stations });
+        this.processQuery(stations);
+      });
+  };
 
-    selectStation = (station) => {
-        const newSelected = this.state.selected.concat(station);
-        this.setState({selected: newSelected});
+  addAmsatSets = () => {
+    this.engine.loadLteFileStations(
+      getCorsFreeUrl('https://www.amsat.org/tle/current/nasabare.txt'),
+      0xffff00,
+    );
+  };
 
-        this.engine.addOrbit(station);
-    }
+  handleTimer = () => {
+    // By default, update in realtime every second, unless dateSlider is displayed.
+    if (!UseDateSlider) this.handleDateChange(null, new Date());
+  };
 
-    deselectStation = (station) => {
-        const newSelected = this.state.selected.filter(s => s !== station);
-        this.setState( { selected: newSelected } );
+  handleSearchResultClick = (station) => {
+    if (!station) return;
 
-        this.engine.removeOrbit(station);
-    }
+    this.toggleSelection(station);
+  };
 
-    addStations = () => {
-        this.addCelestrakSets();
-        //this.engine.addSatellite(ISS);
-        //this.addAmsatSets();
-    }
+  handleRemoveSelected = (station) => {
+    if (!station) return;
 
-    addCelestrakSets = () => {
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/weather.txt'), 0x00ffff)
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/cosmos-2251-debris.txt'), 0xff0090)
-        this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle'), 0xffffff)        
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/science.txt'), 0xffff00)
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/stations.txt'), 0xffff00)
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/iridium-NEXT.txt'), 0x00ff00)
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/gps-ops.txt'), 0x00ff00)
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/ses.txt'), 0xffffff)
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/starlink.txt'), 0x0000ff)
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/gps-ops.txt'), 0xffffff, { orbitMinutes: 0, satelliteSize: 200 })
-        //this.engine.loadLteFileStations(getCorsFreeUrl('https://celestrak.org/NORAD/elements/glo-ops.txt'), 0xff0000, { orbitMinutes: 500, satelliteSize: 500 })
-            .then(stations => {
-                this.setState({stations});
-                this.processQuery(stations);
-            });
+    this.deselectStation(station);
+  };
 
-    }
+  handleRemoveAllSelected = () => {
+    this.state.selected.forEach((s) => this.engine.removeOrbit(s));
+    this.setState({ selected: [] });
+  };
 
-    addAmsatSets = () => {
-        this.engine.loadLteFileStations(getCorsFreeUrl('https://www.amsat.org/tle/current/nasabare.txt'), 0xffff00);
-    }
+  handleReferenceFrameChange = () => {
+    this.state.selected.forEach((s) => this.engine.removeOrbit(s));
 
-    handleTimer = () => {
-        // By default, update in realtime every second, unless dateSlider is displayed.
-        if (!UseDateSlider) this.handleDateChange(null, new Date());
-    }
+    const newType = this.state.referenceFrame === 1 ? 2 : 1;
+    this.setState({ referenceFrame: newType });
+    this.engine.setReferenceFrame(newType);
 
-    handleSearchResultClick = (station) => {
-        if (!station) return;
+    this.state.selected.forEach((s) => this.engine.addOrbit(s));
+  };
 
-        this.toggleSelection(station);
-    }
+  handleDateChange = (v, d) => {
+    const newDate = v ? v.target.value : d;
+    this.setState({ currentDate: newDate });
 
-    handleRemoveSelected = (station) => {
-        if (!station) return;
-        
-        this.deselectStation(station);
-    }
+    const date = new Date();
+    date.setTime(newDate);
+    this.engine.updateAllPositions(date);
+  };
 
-    handleRemoveAllSelected = () => {
-        this.state.selected.forEach(s => this.engine.removeOrbit(s));
-        this.setState({selected: []});
-    }
+  renderDate = (v) => {
+    const result = new Date();
+    result.setTime(v);
+    return result.toString();
+  };
 
-    handleReferenceFrameChange = () => {
-        this.state.selected.forEach(s => this.engine.removeOrbit(s));
+  render() {
+    const { selected, stations, initialDate, currentDate } = this.state;
 
-        const newType = this.state.referenceFrame === 1 ? 2 : 1;
-        this.setState({referenceFrame: newType});
-        this.engine.setReferenceFrame(newType);
+    const maxMs = initialDate + DateSliderRangeInMilliseconds;
 
-        this.state.selected.forEach(s => this.engine.addOrbit(s));
-    }
-
-    handleDateChange = (v, d) => {
-        const newDate = v ? v.target.value : d;
-        this.setState({ currentDate: newDate });
-
-        const date = new Date();
-        date.setTime(newDate);
-        this.engine.updateAllPositions(date);
-    }
-
-    renderDate = (v) => {
-        const result = new Date();
-        result.setTime(v);
-        return result.toString();
-    }
-
-    render() {
-        const { selected, stations, initialDate, currentDate } = this.state;
-
-        const maxMs = initialDate + DateSliderRangeInMilliseconds;
-
-        return (
-            <div>
-                <Fork />
-                <Highlights query={this.state.query} total={this.state.queryObjectCount} />
-                <Info stations={stations} refMode={this.state.referenceFrame} />
-                <Search stations={this.state.stations} onResultClick={this.handleSearchResultClick} />
-                <SelectedStations selected={selected} onRemoveStation={this.handleRemoveSelected} onRemoveAll={this.handleRemoveAllSelected} />
-                {UseDateSlider && <DateSlider min={initialDate} max={maxMs} value={currentDate} onChange={this.handleDateChange} onRender={this.renderDate} />}
-                <div ref={c => this.el = c} style={{ width: '99%', height: '99%' }} />
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Highlights
+          query={this.state.query}
+          total={this.state.queryObjectCount}
+        />
+        <Info stations={stations} refMode={this.state.referenceFrame} />
+        <Search
+          stations={this.state.stations}
+          onResultClick={this.handleSearchResultClick}
+        />
+        <SelectedStations
+          selected={selected}
+          onRemoveStation={this.handleRemoveSelected}
+          onRemoveAll={this.handleRemoveAllSelected}
+        />
+        {UseDateSlider && (
+          <DateSlider
+            min={initialDate}
+            max={maxMs}
+            value={currentDate}
+            onChange={this.handleDateChange}
+            onRender={this.renderDate}
+          />
+        )}
+        <div
+          ref={(c) => (this.el = c)}
+          style={{ width: '99%', height: '99%' }}
+        />
+      </div>
+    );
+  }
 }
-
-
 
 export default App;
